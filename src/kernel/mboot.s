7d4f01dc21b2ff90b98f59115c1f7763dee13083
Ian Laird <irlaird@gmail.com>
2014-05-31 12:05:08 -0600

    Added multiboot header

--- src/kernel/mboot.s
+++ src/kernel/mboot.s
@@ -1,3 +1,27 @@
-.global _start
-.section .text
-_start: jmp _start
+                .global _start
+
+                ## Multiboot Flags
+                .set ALIGN, 1<<0
+                .set MEMINFO, 1<<1
+
+                ## Compute Multiboot header values
+                .set FLAGS, ALIGN | MEMINFO
+                .set MAGIC, 0x1BADB002
+                .set CHECKSUM, -(MAGIC + FLAGS)
+
+
+                .section .multiboot
+
+                ## Multiboot Header
+
+                .align 4
+                .long MAGIC
+                .long FLAGS
+                .long CHECKSUM
+
+
+                .section .text
+
+                ## Kernel Entry 
+
+_start:         jmp _start
