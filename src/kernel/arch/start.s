e5cab86ed23ed3f8f2290758c277b580d2b3db03
Ian Laird <irlaird@gmail.com>
2017-07-21 07:49:59 -0600

    Cleaned up the start.s

--- src/kernel/arch/start.s
+++ src/kernel/arch/start.s
@@ -16,6 +16,8 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+        .set VALID_MAGIC, 0x2BADB002
+
         .global _start
         .global kabort
         .extern initIDT
@@ -24,21 +26,23 @@
         .extern kprintf
         .extern kclear
 
-        .set VALID_MAGIC, 0x2BADB002
 
         .section .tstack, "aw", @nobits
+
         .skip 4096              ## 4KB of stack space.
 stack:  nop
 
-        .section .bss
-mbi:    .long 0x0
 
         .section .rodata
+
 MSGERR: .asciz "System failed to boot. I dont know why.\n"
 MSGHTL: .asciz "System Halted.\n"
 MSGFTL: .asciz "Ooh Snap!\n\nYour bootloader does not appear to be multiboot complient.\n"
 
+
         .section .text
+        .align 4
+
 _start: movl $stack, %esp       ## Setup temp stack
         pushl $0                ## Reset EFLAGS
         popf
@@ -48,17 +52,14 @@ _start: movl $stack, %esp       ## Setup temp stack
         push $MSGFTL            ## eax was not the expected value.
         call kabort             ## Abort with message
 
-.start: mov %ebx, (mbi)         ## Backup Multiboot Info
-
+.start: push %ebx               ## EBX points to multiboot_info_t
         call kclear             ## Clear the screen
-        push mbi                ## We have a new stack, Repush the mbi
         call system_init        ## Call system_init(mboot*)
         call main               ## Call main(mboot*)
 
-        ## Failed Boot
+        ## This should never happen
         push $MSGERR
         call kabort
-
 
 kabort: push %ebp
         mov %esp, %ebp
